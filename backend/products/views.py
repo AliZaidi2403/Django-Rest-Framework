50c084b2568f6ef5b9a2af6101fc605d343d7f9a
Ali Zaidi <amaanzaidi462@gmail.com>
2025-08-29 17:42:41 +0530

    Mixins and Generic API view

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
@@ -36,6 +36,8 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
         #instance -> object that is going to be deleted
         super().perform_destroy(instance)   #call the parent class's perform_destroy method to actually delete the object.
 
+
+
 class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -57,6 +59,28 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
         if not instance.content:
             instance.content = instance.title
             instance.save(update_fields=["content"])
+
+
+class ProductMixinView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs) #detail view
+        return self.list(request, *args, **kwargs) #list view
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
 
 @api_view(['GET', 'POST'])
 def product_alt_view(request,pk=None, *args, **kwargs):
