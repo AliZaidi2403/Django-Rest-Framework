004da822a7522e28339e19167fcb8f35319c9a5a
Ali Zaidi <amaanzaidi462@gmail.com>
2025-08-29 14:46:50 +0530

    Second Commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,9 @@ from django.http import JsonResponse
 import json
 from products.models import Product 
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.serializers import ProductSerializer
 
 def first_api_home(request, *args, **kwargs):
     # request -> HttpRequest object -> Django
@@ -14,7 +17,7 @@ def first_api_home(request, *args, **kwargs):
     except :
         pass
     print(data) # {'query': 'hello world'}
-    data  = dict(request.GET)
+    data  = dict(request.data)
     data['headers'] = dict(request.headers)
     data['content_type'] = request.content_type 
     print(data)
@@ -27,10 +30,28 @@ def first_api_home(request, *args, **kwargs):
 
 
 
-def api_home(request, *args, **kwargs): 
+def second_api_home(request, *args, **kwargs): 
+    if request.method == "POST" : 
+        return JsonResponse({"message": "POST request not allowed."}, status=405)
     model_data = Product.objects.all().order_by("?").first()
     data  = {} 
     if model_data : 
         data = model_to_dict(model_data, fields=['id', 'title', 'content', 'price'])
+        data['sale_price'] = model_data.sale_price 
     return JsonResponse(data)
+
+@api_view(['GET','POST']) #now this will do the basic method checking for us
+def api_home(request, *args, **kwargs):
+    """Django REST Framework"""
+    if request.method == "POST" : 
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            # instance = serializer.save()
+            print(serializer.data)
+            return Response(serializer.data)
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance :
+        data = ProductSerializer(instance).data
+    return Response(data)
 
